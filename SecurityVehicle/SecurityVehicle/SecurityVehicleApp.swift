859e32e4f3f729be2dbc2d840ef395101b2e40d0
Fatima Victoria Urpi Chavez <143281970+FatimaUrpi@users.noreply.github.com>
2024-11-30 09:09:54 -0500

    Add files via upload

---
+++ SecurityVehicle/SecurityVehicle/SecurityVehicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityVehicleApp.swift
+//  SecurityVehicle
+//
+//  Created by DAMII on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SecurityVehicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
