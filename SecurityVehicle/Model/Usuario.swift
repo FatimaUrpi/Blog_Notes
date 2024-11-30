12c7517b4fb4847a0a1a519556109b59aeb8f507
ZarelaMC <zarelamanrique87@gmail.com>
2024-11-30 09:43:54 -0500

    Feat:Atributos_ModelUsuario

--- SecurityVehicle/Model/Usuario.swift
+++ SecurityVehicle/Model/Usuario.swift
@@ -1,8 +1,11 @@
-//
-//  Usuario.swift
-//  SecurityVehicle
-//
-//  Created by DAMII on 30/11/24.
-//
-
 import Foundation
+import CoreData
+
+class Usuario: NSManagedObject, Identifiable {
+    @NSManaged var idUsuario: Int
+    @NSManaged var username: String?
+    @NSManaged var password: String?
+    @NSManaged var correo: String?
+    @NSManaged var nombre: String?
+}
+
