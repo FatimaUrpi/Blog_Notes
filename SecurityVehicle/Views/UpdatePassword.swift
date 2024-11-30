e0664550c84eda3fae295b380dc18390019b5464
Fatima Victoria Urpi Chavez <143281970+FatimaUrpi@users.noreply.github.com>
2024-11-30 11:28:09 -0500

    FEAT: updatePassword en proceso

--- SecurityVehicle/Views/UpdatePassword.swift
+++ SecurityVehicle/Views/UpdatePassword.swift
@@ -1,106 +1,94 @@
+//
+//  UpdatePassword.swift
+//  SecurityVehicle
+//
+//  Created by DAMII on 30/11/24.
+//
+
 import SwiftUI
 
-struct LoginView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
-    @State private var isSecure: Bool = true
-    @State private var showAlert: Bool = false
-    @State private var alertMessage: String = ""
-    @State private var isLoggedIn: Bool = false
+struct UpdatePassword: View {
+    @State private var correo: String = ""
+       @State private var nuevaContrasena: String = ""
+       @State private var confirmarContrasena: String = ""
+       @State private var mostrarNuevaContrasena: Bool = false
+       @State private var mostrarConfirmarContrasena: Bool = false
+       @State private var mensaje: String = ""
+       @State private var mostrarMensaje: Bool = false
 
-    var body: some View {
-        NavigationView {
-            VStack {
-                Image(systemName: "note.text") // Icono de notas
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.blue)
-                    .padding(.bottom, 40)
-                
-                Text("Bienvenido")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.primary)
-                    .padding(.bottom, 20)
+       var body: some View {
+           VStack(spacing: 20) {
+               Text("Actualizar Contraseña")
+                   .font(.largeTitle)
+                   .padding()
 
-                TextField("Username", text: $username)
-                    .textInputAutocapitalization(.never)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder(Color.gray.opacity(0.5)))
-                    .padding(.bottom, 20)
+               // Campo de correo
+               TextField("Correo Electrónico", text: $correo)
+                   .textFieldStyle(RoundedBorderTextFieldStyle())
+                   .padding(.horizontal)
 
-                HStack {
-                    if isSecure {
-                        SecureField("Contraseña", text: $password)
-                            .textInputAutocapitalization(.never)
-                    } else {
-                        TextField("Contraseña", text: $password)
-                            .textInputAutocapitalization(.never)
-                    }
-                    Button(action: {
-                        isSecure.toggle()
-                    }) {
-                        Image(systemName: isSecure ? "eye.slash" : "eye")
-                            .foregroundColor(.blue)
-                    }
-                }
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 10).strokeBorder(Color.gray.opacity(0.5)))
+               // Campo de nueva contraseña con botón de visibilidad
+               HStack {
+                   if mostrarNuevaContrasena {
+                       TextField("Nueva Contraseña", text: $nuevaContrasena)
+                   } else {
+                       SecureField("Nueva Contraseña", text: $nuevaContrasena)
+                   }
+                   Button(action: {
+                       mostrarNuevaContrasena.toggle()
+                   }) {
+                       Image(systemName: mostrarNuevaContrasena ? "eye.slash" : "eye")
+                           .foregroundColor(.gray)
+                   }
+               }
+               .textFieldStyle(RoundedBorderTextFieldStyle())
+               .padding(.horizontal)
 
-                NavigationLink(destination: HomeView(), isActive: $isLoggedIn) {
-                    EmptyView()
-                }
+               // Campo de confirmar contraseña con botón de visibilidad
+               HStack {
+                   if mostrarConfirmarContrasena {
+                       TextField("Confirmar Contraseña", text: $confirmarContrasena)
+                   } else {
+                       SecureField("Confirmar Contraseña", text: $confirmarContrasena)
+                   }
+                   Button(action: {
+                       mostrarConfirmarContrasena.toggle()
+                   }) {
+                       Image(systemName: mostrarConfirmarContrasena ? "eye.slash" : "eye")
+                           .foregroundColor(.gray)
+                   }
+               }
+               .textFieldStyle(RoundedBorderTextFieldStyle())
+               .padding(.horizontal)
 
-                Button(action: {
-                    if username.isEmpty || password.isEmpty {
-                        alertMessage = "Por favor, ingresa tus credenciales."
-                        showAlert = true
-                    } else if UsuarioManager.shared.validarUsuario(username: username, password: password) {
-                        alertMessage = "Inicio de sesión exitoso."
-                        showAlert = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            isLoggedIn = true
-                        }
-                    } else {
-                        alertMessage = "Credenciales incorrectas. Intenta nuevamente."
-                        showAlert = true
-                    }
-                }) {
-                    Text("Iniciar sesión")
-                    
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding(.top, 20)
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Mensaje"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
+               // Botón para actualizar contraseña
+               Button(action: {
+                   // Acción de actualización de contraseña
+                   mensaje = "Contraseña actualizada con éxito."
+                   mostrarMensaje = true
+               }) {
+                   Text("Actualizar Contraseña")
+                       .frame(maxWidth: .infinity)
+                       .padding()
+                       .background(Color.blue)
+                       .foregroundColor(.white)
+                       .cornerRadius(10)
+               }
+               .padding(.horizontal)
 
-                Spacer()
+               // Mostrar mensaje de éxito o error
+               if mostrarMensaje {
+                   Text(mensaje)
+                       .foregroundColor(.green)
+                       .padding()
+               }
 
-                HStack {
-                    Text("¿No tienes una cuenta?")
-                    NavigationLink(destination: RegisterView()) {
-                        Text("Regístrate")
-                            .foregroundColor(.blue)
-                            .fontWeight(.bold)
-                    }
-                }
-                .padding(.top, 20)
-            }
-            .padding()
-            .background(Color(.systemGray6).edgesIgnoringSafeArea(.all))
-        }
-    }
-}
+               Spacer()
+           }
+           .padding()
+       }
+   }
 
-
-
-#Preview {
-    LoginView()
-}
+   #Preview {
+       UpdatePassword()
+   }
