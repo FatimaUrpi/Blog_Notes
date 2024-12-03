3c27fd408b6188cdf6838d2cacf72558047327d0
YheremiRamos <yheremiramosquispe@gmail.com>
2024-12-03 09:13:51 -0500

    Featt: generales

--- SecurityVehicle/Views/HomeView.swift
+++ SecurityVehicle/Views/HomeView.swift
@@ -1,15 +1,64 @@
-//
-//  HomeView.swift
-//  SecurityVehicle
-//
-//  Created by DAMII on 30/11/24.
-//
+import SwiftUI
 
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showLoginView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Fondo colorido
+            Color.blue.edgesIgnoringSafeArea(.all)
+
+            // Contenedor principal
+            VStack {
+                Image("notasPrincipal") // Asegúrate de tener una imagen llamada "notebook" en tus assets
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .padding(.top, 80)
+
+                // Título
+                Text("Descubre Notas App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+
+                // Descripción
+                Text("Organiza tus pensamientos y tareas de manera fácil y eficiente.")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                    .padding(.top, 10)
+
+                Spacer()
+                
+                /*NavigationLink(destination: LoginView()) {
+                 LoginView()
+             }*/
+                // Botón de inicio
+                Button(action: {
+                    showLoginView = true
+                }) {
+                    Text("Comenzar")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+                .padding(.bottom, 40)
+            }
+            .padding()
+            .background(Color.white.opacity(0.4).cornerRadius(20))
+            .shadow(radius: 10)
+            .padding(.horizontal, 20)
+        }
+        .fullScreenCover(isPresented: $showLoginView) {LoginView()}
     }
 }
 
