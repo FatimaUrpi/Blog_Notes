9f1217f80bb2504530e682425f45fcbef89a622c
lesliemarlo <lesliemarlo09@gmail.com>
2024-12-16 14:16:48 -0500

    feat: login diseno

--- SecurityVehicle/Views/LoginView.swift
+++ SecurityVehicle/Views/LoginView.swift
@@ -9,88 +9,174 @@ import SwiftUI
 import FirebaseAuth
 
 struct LoginView: View {
+    @State private var showLoginView = false
+    @State private var showRegisterView = false
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var showPassword: Bool = false
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    
     @State private var errorMessage: String?
     @State private var isAuthenticated = false
-
+    
     var body: some View {
         if isAuthenticated {
             HomeView() // Redirige a la pantalla principal al autenticar
         } else {
-            VStack(spacing: 20) {
-                Text("Iniciar Sesión")
-                    .font(.largeTitle)
-                    .bold()
-
-                TextField("Correo Electrónico", text: $email)
-                    .autocapitalization(.none)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                SecureField("Contraseña", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                if let errorMessage = errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .font(.caption)
+            ZStack {
+                // Fondo principal
+                Color(red: 211 / 255, green: 230 / 255, blue: 249 / 255, opacity: 1)
+                // Color celeste claro
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 20) {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: 20)
+                        .padding(.top, 20)
+                        .padding(.bottom, 100)
+                    
+                    Text("Iniciar Sesión")
+                        .font(.title)
+                        .bold()
+                    
+                    Text("Email:")
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 30)
+                    
+                    TextField("ejemplo@gmail.com", text: $email)
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 30)
+                    Text("Password:")
+                    
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 30)
+                    
+                    ZStack(alignment: .trailing) {
+                        if showPassword {
+                            TextField("##########", text: $password)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .padding(.horizontal, 30)
+                        } else {
+                            SecureField("##########", text: $password)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .padding(.horizontal, 30)
+                        }
+                        
+                        Button(action: {
+                            showPassword.toggle()
+                        }) {
+                            Image(systemName: showPassword ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 40)
+                        }
+                    }
+                    
+                    if let errorMessage = errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .font(.caption)
+                    }
+                    
+                    // Imagen superior
+                    Image("login")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: 400) // Máximo ancho y altura limitada
+                        .padding(.top, 5)
+                        .padding(.bottom, -25)
+                    
+                    Button(action: {
+                        showLoginView = true
+                    }) {
+                        Text("Crea una nota")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white
+    )
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(red: 125 / 255, green: 142 / 255, blue: 215 / 255, opacity: 1)
+                                        
+    ) // Botón morado suave
+                            .cornerRadius(10)
+                            .padding(.horizontal, 50)
+                            .padding(.bottom, 30)
+                    }
+                    .fullScreenCover(isPresented: $showLoginView) {
+                        LoginView()
+                    }
+                    
+                    Button(action: {
+                        showRegisterView = true
+                    }) {
+                        Text("Already have an account?")
+                            .font(.footnote)
+                            .foregroundColor(Color(red: 125 / 255, green: 142 / 255, blue: 215 / 255, opacity: 1)
+    ) // Mismo color que el botón
+                            .padding(.bottom, 30)
+                    }
+                    .fullScreenCover(isPresented: $showRegisterView) {
+                        RegisterView()
+                    }
                 }
-
-                Button("Iniciar Sesión") {
-                    loginUser()
+                .padding()
+            }
+        }}
+        
+        // Función para iniciar sesión
+        private func loginUser() {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    self.errorMessage = handleAuthError(error)
+                    return
                 }
-                .buttonStyle(.borderedProminent)
-
-                Button("Registrarse") {
-                    registerUser()
+                self.isAuthenticated = true
+            }
+        }
+        
+        // Función para registrar un usuario
+        private func registerUser() {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    self.errorMessage = handleAuthError(error)
+                    return
                 }
-                .buttonStyle(.bordered)
+                self.errorMessage = "Usuario registrado con éxito. Por favor, inicia sesión."
             }
-            .padding()
         }
-    }
-
-    // Función para iniciar sesión
-    private func loginUser() {
-        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                self.errorMessage = handleAuthError(error)
-                return
+        
+        //Función auxiliar para errores
+        private func handleAuthError(_ error: Error) -> String {
+            let nsError = error as NSError
+            switch AuthErrorCode(rawValue: nsError.code) {
+            case .invalidEmail:
+                return "El correo electrónico no es válido."
+            case .emailAlreadyInUse:
+                return "El correo ya está en uso."
+            case .weakPassword:
+                return "La contraseña es demasiado débil."
+            case .wrongPassword:
+                return "Contraseña incorrecta."
+            case .userNotFound:
+                return "Usuario no encontrado."
+            default:
+                return error.localizedDescription
             }
-            self.isAuthenticated = true
         }
     }
-
-    // Función para registrar un usuario
-    private func registerUser() {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                self.errorMessage = handleAuthError(error)
-                return
-            }
-            self.errorMessage = "Usuario registrado con éxito. Por favor, inicia sesión."
-        }
-    }
-    
-    //Función auxiliar para errores
-    private func handleAuthError(_ error: Error) -> String {
-        let nsError = error as NSError
-        switch AuthErrorCode(rawValue: nsError.code) {
-        case .invalidEmail:
-            return "El correo electrónico no es válido."
-        case .emailAlreadyInUse:
-            return "El correo ya está en uso."
-        case .weakPassword:
-            return "La contraseña es demasiado débil."
-        case .wrongPassword:
-            return "Contraseña incorrecta."
-        case .userNotFound:
-            return "Usuario no encontrado."
-        default:
-            return error.localizedDescription
-        }
-    }
-}
 
 
 #Preview {
